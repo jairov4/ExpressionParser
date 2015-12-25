350d27a2cd990ce3f254fa62d6309d6723a6f345
jairov <jairo_andres_v@hotmail.com>
2015-12-25 10:05:56 -0500

    Added measurement unit resolver

---
+++ ExpressionParser/MeasurementUnitResolver.cs
@@ -0,0 +1,54 @@
+﻿using DXAppProto2.FilterExpressions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DXAppProto2
+{
+    public class MeasurementUnitResolver
+    {
+        public MeasurementUnitResolver()
+        {
+        }
+
+        public IMeasurementUnit GetMeasurementUnit(IMeasurementUnitRepository repo)
+        {
+
+        }
+
+        struct FilterExpressionVisitor : IFilterExpressionVisitor
+        {
+            public void Visit(FilterExpressionCastNode node)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Visit(FilterExpressionLiteralNode node)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Visit(FilterExpressionMethodCallNode node)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Visit(FilterExpressionFieldReferenceNode node)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Visit(FilterExpressionUnaryNode node)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Visit(FilterExpressionBinaryNode node)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
